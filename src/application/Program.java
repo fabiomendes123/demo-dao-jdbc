7f91384ff040629d80999c90ad0cc29a1df379e8
fabiomendes123 <fabiomendes@live.com>
2020-06-05 12:04:29 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,7 @@ public class Program {
 		
 		Department obj = new Department (1,"Books");
 		Seller seller = new Seller (21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
-				
-		System.out.println(obj);
+		
 		System.out.println(seller);
 	}
 
