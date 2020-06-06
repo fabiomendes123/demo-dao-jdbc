51815f5853f4f2825cb29dee1e6d0cb85997ee14
fabiomendes123 <fabiomendes@live.com>
2020-06-06 12:57:03 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,19 +19,25 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== TESTE NR 1: Seller findByID =====");
+		System.out.println("\n=== TESTE NR 2: Seller findDepartment =====");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TESTE NR 3: Seller findAll =====");
+		/*System.out.println("\n=== TESTE NR 3: Seller insertSeller =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New Id= " + newSeller.getId());
+		System.out.println("Inserted! New Id= " + newSeller.getId());*/
 		
-		System.out.println("\n=== TESTE NR 3: Seller findAll =====");
+		System.out.println("\n=== TESTE NR 5: Seller updateSeller =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update( seller);
+		System.out.println("Update Copleted");
+		
+		System.out.println("\n=== TESTE NR 4: Seller findAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
